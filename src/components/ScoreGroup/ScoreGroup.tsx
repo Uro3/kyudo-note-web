cf06a54e63505880ce63f5229f4b4d21e0132dfd
Uro3 <miyakawa.uro3@gmail.com>
2019-12-30 15:24:53 +0900

    add function to save data in localstorage

--- src/components/ScoreGroup/ScoreGroup.tsx
+++ src/components/ScoreGroup/ScoreGroup.tsx
@@ -1,13 +1,35 @@
 import * as React from 'react';
 import ScoreUnit from '../ScoreUnit';
 
-const ScoreGroup: React.FC = () => {
-  const [scores, setScores] = React.useState([[0, 0, 0, 0]]);
+const storageName = 'scores';
 
-  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+const ScoreGroup: React.FC = () => {
+  let initialData = [[0, 0, 0, 0]];
+  const json = localStorage.getItem(storageName);
+  if (json) {
+    try {
+      initialData = JSON.parse(json);
+      if (!Array.isArray(initialData) || initialData.length === 0) {
+        initialData = [[0, 0, 0, 0]];
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  const [scores, setScores] = React.useState(initialData);
+
+  const onClickAdd = (event: React.MouseEvent<HTMLButtonElement>) => {
     const newScores = [...scores];
-    newScores.push([0,0,0,0]);
+    newScores.push([0, 0, 0, 0]);
     setScores(newScores);
+  }
+
+  const onClickSave = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const savedScores = scores.filter(values => 
+      values.some(value => value)
+    );
+    const data = JSON.stringify(savedScores);
+    localStorage.setItem(storageName, data);
   }
 
   const onScoreChanged = (index1: number, index2: number, nextValue: number) => {
@@ -23,7 +45,8 @@ const ScoreGroup: React.FC = () => {
   return (
     <div>
       {scoreUnits}
-      <button onClick={onClick}>追加</button>
+      <button onClick={onClickAdd}>追加</button>
+      <button onClick={onClickSave}>一時保存</button>
     </div>
   );
 };
