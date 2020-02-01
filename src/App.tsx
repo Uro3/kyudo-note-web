cf69b642587bcc7bc879be7b2c9312acd628af1f
Uro3 <miyakawa.uro3@gmail.com>
2020-02-01 17:21:34 +0900

    add router sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import * as React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import {ScoreGroupContainer} from './components/ScoreGroup';
+import {Login} from './components/Auth';
 
 const App: React.FC = () => {
   return (
-    <ScoreGroupContainer />
+    <Router>
+      <ul>
+        <li><Link to="/">HOME</Link></li>
+        <li><Link to="/login">LOGIN</Link></li>
+      </ul>
+      <hr/>
+      <Switch>
+        <Route exact path='/' component={ScoreGroupContainer}></Route>
+        <Route path='/login' component={Login}></Route>
+      </Switch>
+    </Router>
   );
 };
 
---
+++ src/components/Auth/Login.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const Login: React.FC<{}> = () => {
+  return (
+    <div>
+      Hello
+    </div>
+  );
+};
+
+export default Login;
