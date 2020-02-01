c52dd7cddf015ce1dc345ae742a6ecae13edacf6
Uro3 <miyakawa.uro3@gmail.com>
2020-02-01 18:09:05 +0900

    add login form

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -1,10 +1,35 @@
 import * as React from 'react';
 
 const Login: React.FC<{}> = () => {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const onEmailChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+  const onPasswordChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(`
+      email: ${email}
+      password: ${password}
+    `);
+  };
+
   return (
-    <div>
-      Hello
-    </div>
+    <form onSubmit={onSubmit}>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={onEmailChanged} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={onPasswordChanged}/>
+      </label>
+      <input type="submit" value="ログイン"/>
+    </form>
   );
 };
 
